9ff2343824c44232ba2b70b3d08041ade119b752
Oleg Bugaevich <olegbugaevich@gmail.com>
2022-02-08 23:51:38 +0300

    init my app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ToggleButton from "./components/toggleButton";
 const App = () => {
   const [buttonText, setButtonText] = useState('Click me please!');
   const [toggle, setToggle] = useState(false);
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <MyButton changeText={setButtonText}>{buttonText}</MyButton>
@@ -14,6 +15,12 @@ const App = () => {
         >
           {toggle ? 'One' : 'Two'} 
         </ToggleButton>
+      <h2>{count}</h2>
+      <button className="plus" onClick={() => setCount(count + 1)}>Plus</button>
+      <button className="minus" onClick={() => setCount(count - 1)}
+                                disabled={count === 0}>Minus</button>
+      <button className="reset" onClick={() => setCount(0)}
+                                disabled={count === 0}>Reset</button>
     </div>
   );
 }
